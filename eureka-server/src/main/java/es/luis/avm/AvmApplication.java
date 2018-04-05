f956275ede1f4a856edcb210c172f86d2f0de678
luisordonezcosta <sdmlordone1@gmail.com>
2018-04-05 19:23:20 +0200

    init

---
+++ eureka-server/src/main/java/es/luis/avm/AvmApplication.java
@@ -0,0 +1,12 @@
+package es.luis.avm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvmApplication.class, args);
+	}
+}
---
+++ eureka-server/src/test/java/es/luis/avm/AvmApplicationTests.java
@@ -0,0 +1,16 @@
+package es.luis.avm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
